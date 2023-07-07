a9ec8db18b4f2a80a3a4e7f02ce675c79fac115c
Wendy013 <125756617+Wendy013@users.noreply.github.com>
2023-07-07 04:41:26 -0500

    Update copier.py

--- copier.py
+++ copier.py
@@ -1,13 +1,18 @@
 import os
 import shutil
 
-class copy_files:
-    def filecopier(files, output):
+def copy_folder(entrance, output):
+    files_img = []
+
+    for root, dirs, files in os.walk(entrance):
         for file in files:
-            name = os.path.basename(file)
-            folder = name.split(".")[0]
-            os.makedirs(os.path.join(output,folder), exist_ok=True)
-            shutil.copy(file, os.path.join(output,folder,name))
+            files_img.append(os.path.join(root, file))
+
+    for file in files_img:
+        name = os.path.basename(file)
+        folder = name.split(".")[0]
+        os.makedirs(os.path.join(output, folder), exist_ok=True)
+        shutil.copy(file, os.path.join(output, folder, name))
 
 
 
